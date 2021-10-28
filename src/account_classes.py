e0ae971c96128c4c20962e366d7bb22357601153
Elijah <elijah.thomas.cox@gmail.com>
2021-10-28 01:46:54 -0500

    move accounts, add function to checking acc

--- src/account_classes.py
+++ src/account_classes.py
@@ -1,20 +1,30 @@
-class Account:
+class Account: #Abstract account class
     def __init__(name):
         self.ID = 0
         self.balance = 0
         self.Name = name
 
-    def deposit(amount):
+    def deposit(amount): #encapsulation to protect balance with getter/setter
         self.balance += amount
     
-    def withdraw(amount):
-        self.balance -= amount
+    def withdraw(amount):#encapsulation to protect balance with getter/setter
+        if balance - amount >= 0:
+            self.balance -= amount
 
-    def check_balance():
+    def check_balance():#encapsulation to protect balance with getter/setter
         return self.balance
 
-class Checking(Account):
-    pass
+class Checking(Account): #property of inheritance: checking derives properties from Account
+    def __init__(name,overdraft_fee:int):
+        super().__init__(name)
+        self.overdraft_fee = overdraft_fee
+    def withdraw(amount):
+        self.balance -= amount
+        if balance < 0:
+            balance -= overdraft_fee
 
-class Savings(Account):
-    pass
+
+class Savings(Account): #property of inheritance: checking derives properties from Account
+    def __init__(name,interest_rate):
+        super().__init__(name)
+        self.interest_rate = interest_rate
