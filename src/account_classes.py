5127bcecadae5b8657c6b8abab7fb74ac4b571e2
Elijah <elijah.thomas.cox@gmail.com>
2021-11-01 00:37:50 -0500

    add get_acc_by_num function

--- src/account_classes.py
+++ src/account_classes.py
@@ -1,5 +1,6 @@
 from random import seed
 from random import randint
+from datetime import date
 seed(1)
 
 class BankManager:
@@ -10,13 +11,22 @@ class BankManager:
             self.checking_accounts[acc.ID] = acc
         else:
             self.savings_accounts[acc.ID] = acc
+    
+    def get_account_by_num(self,num):
+        if num[0] == 'C':
+            if num in self.checking_accounts:
+                return self.checking_accounts[num]
+        elif num[0] == 'S':
+            if num in self.savings_accounts:
+                return self.savings_accounts[num]        
 
 class Account: #Abstract account class
     def __init__(self,name,account_type):
         self.ID = account_type+str(randint(10,99))
         self.balance = 0
         self.name = name
-   
+        self.created_date = date.today().strftime("%d/%m/%Y")
+
     def deposit(self,amount): #encapsulation to protect balance with getter/setter
         self.balance += amount
     
